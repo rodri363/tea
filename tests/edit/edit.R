5658cd714ddad37d3339a9a08a2be2818cb75504
rrod <rrod@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-08-29 14:56:03 +0000

    added simple editing example
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/tea@76 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ tests/edit/edit.R
@@ -0,0 +1,17 @@
+library(tea) #load up TEA
+set.seed(5318008) #fix the random number generator
+read_spec("spec") #read in the spec; this primes the consistency checking system
+doInput() #input the data as described in the input{} section of the spec
+
+#read the data into an R data frame
+DF <- dbGetQuery(pepenv$con,"select * from data")
+#grab the edit variables from the database
+vedvar <- dbGetQuery(pepenv$con,"select * from variables")$name
+#two different ways to see if each row of the data passes all edits
+va <- as.integer(CheckDF(DF,pepenv$con))
+vb <- as.integer(.Call("r_check_a_table",DF)$Vector)
+#a list containing the viable edit alternatives for each data row
+#NULL means the row passed all edits
+Le <- sapply(1:nrow(DF),function(kr)
+	return(CheckConsistency(DF[kr,vedvar],vedvar,"find_alternatives",pepenv$con)))
+print(Le)
