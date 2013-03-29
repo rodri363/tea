0b7ee3a71b90cd23bb5ce40e17e3050d8745ded0
dvernet <dvernet@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-03-29 15:08:46 +0000

    Updated tutorial.tex with inline dot files. There's a strange error happening where pdflatex won't compile anything past a \comment{...} command which we need to fix. In any case, after adding captions the dotFiles should be good to go in the tutorial document.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/tea@155 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- doc/current_tutorial_documents/dotFiles/dotScript.sh
+++ doc/current_tutorial_documents/dotFiles/dotScript.sh
@@ -1,5 +1,6 @@
 cat dotInput >> connected_dot_file
 cat dotFields >> connected_dot_file
 cat dotChecks >> connected_dot_file
+cat dotRecodes >> connected_dot_file
 cat dotImpute >> connected_dot_file
 dot -Tpng < connected_dot_file > connected_dot.png
