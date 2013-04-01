ad985f6177c0fd5f9b61b637515b2152700c127e
dvernet <dvernet@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-04-01 12:31:20 +0000

    Updated dotFiles that are used in the script dotScript.sh to compile the larger dotFile by combining smaller dot files for each major key in spec file. I also added a short README file to describe the differences between the dot files in /tea/doc/current_tutorial_documents/dotFiles and the dot files in /tea/doc/current_tutorial_documents and updated dotScript.sh itself to reflect the changes made. It's been tested and compiles correctly on my machine.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/tea@157 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- doc/current_tutorial_documents/dotFiles/dotScript.sh
+++ doc/current_tutorial_documents/dotFiles/dotScript.sh
@@ -1,6 +1,9 @@
-cat dotInput >> connected_dot_file
-cat dotFields >> connected_dot_file
-cat dotChecks >> connected_dot_file
-cat dotRecodes >> connected_dot_file
-cat dotImpute >> connected_dot_file
+echo 'digraph { ' > connected_dot_file
+echo 'node [shape=Mrecord];' >> connected_dot_file
+cat dotInput_scr >> connected_dot_file
+cat dotFields_scr >> connected_dot_file
+cat dotChecks_scr >> connected_dot_file
+cat dotRecodes_scr >> connected_dot_file
+cat dotImpute_scr >> connected_dot_file
+echo '}' >> connected_dot_file
 dot -Tpng < connected_dot_file > connected_dot.png
