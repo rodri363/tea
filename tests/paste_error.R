bfd014950155f00d61822692dc06f6f7fa677efa
Ned Porter <porte001@census.gov>
2014-09-23 11:29:04 -0400

    Updated paste_error files .R added tryCatch .spec added comments

--- tests/paste_error.R
+++ tests/paste_error.R
@@ -1,4 +1,33 @@
 library(tea)
-readSpec("paste_error.spec")
+tryCatch({
+  tc <- readSpec("paste_error.spec")
+  }, interrupt = function(ex) {
+    cat("An Interrupt was detected\n");
+    print(ex);
+    quit();
+  }, error = function(ex) {
+    cat("An Error was detected\n");
+    print(ex);
+    quit();
+  }, warning = function(ex) {
+    cat("A Warning was detected\n");
+    print(ex);
+    quit();
+})
+
 doInput()
-doMImpute()
+tryCatch({
+  ty <- doMImpute()
+  }, interrupt = function(ex) {
+    cat("An Interrupt was detected\n");
+    print(ex);
+    quit();
+  }, error = function(ex) {
+    cat("An Error was detected\n");
+    print(ex);
+    quit();
+  }, warning = function(ex) {
+    cat("A Warning was detected\n");
+    print(ex);
+    quit();
+})
