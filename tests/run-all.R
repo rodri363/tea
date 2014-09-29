bca22e5ac24aad76fb6acf662ae959d6893124ba
Ned Porter <porte001@census.gov>
2014-09-29 16:39:52 -0400

    Update run-all.R -- added tryCatch for the unknown tea test

--- tests/run-all.R
+++ tests/run-all.R
@@ -1,2 +1,16 @@
 library(tea)
-test_package("tea")
+tryCatch({
+ tc <- test_package("tea")
+ }, interrupt = function(ex) {
+    cat("An Interrupt was detected run-all test_package(\"tea\")\n");
+    print(ex);
+    quit();
+  }, error = function(ex) {
+    cat("An Error was detected run-all test_package(\"tea\")\n");
+    print(ex);
+    quit();
+  }, warning = function(ex) {
+    cat("A Warning was detected run-all test_package(\"tea\")\n");
+    print(ex);
+    quit();
+})
