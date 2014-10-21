5835c932dd1402b61b4c3ed17385014d0158d02c
Ned Porter <porte001@census.gov>
2014-10-21 15:50:07 -0400

    Revert "weight.R -- tryCatch added before weighting functionality implemented" Restored previous readme-md This reverts commit 9b83af1ea52a16332a86b31246c5203c103a581b.

--- tests/weight.R
+++ tests/weight.R
@@ -1,18 +1,4 @@
 library(tea)
-tryCatch({
- tc <- readSpec("weight.spec")
- }, interrupt = function(ex) {
-    cat("An Interrupt was detected in read_spec\n");
-    print(ex);
-    quit();
-  }, error = function(ex) {
-    cat("An Error was detected in read_spec\n");
-    print(ex);
-    quit();
-  }, warning = function(ex) {
-    cat("A Warning was detected in read_spec\n");
-    print(ex);
-    quit();
-})
+readSpec("weight.spec")
 doInput()
 doMImpute()
