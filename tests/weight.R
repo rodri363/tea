2739ebf61e658ae848fafe9705df9d2cd6e570e9
Ned Porter <porte001@census.gov>
2014-10-21 16:28:06 -0400

    weight.R corrected

--- tests/weight.R
+++ tests/weight.R
@@ -1,4 +1,33 @@
 library(tea)
-readSpec("weight.spec")
+tryCatch({
+ tc <- readSpec("weight.spec")
+  }, interrupt = function(ex) {
+    cat("An Interrupt was detected readSpec\n");
+    print(ex);
+    quit();
+  }, error = function(ex) {
+    cat("An Error was detected readSpec\n");
+    print(ex);
+    quit();
+  }, warning = function(ex) {
+    cat("A Warning was detected readSpec\n");
+    print(ex);
+    quit();
+})
+
 doInput()
-doMImpute()
+tryCatch({
+  th <- doMImpute()
+ }, interrupt = function(ex) {
+    cat("An Interrupt was detected doMImpute\n");
+    print(ex);
+    quit();
+  }, error = function(ex) {
+    cat("An Error was detected doMImpute\n");
+    print(ex);
+    quit();
+  }, warning = function(ex) {
+    cat("A Warning was detected doMImpute\n");
+    print(ex);
+    quit();
+})
